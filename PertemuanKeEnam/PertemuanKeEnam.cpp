02f9b87880848b2e1979d6a53ef48bfcc79758f2
rcslbaa <ifasmarani3125@gmail.com>
2024-03-28 17:46:05 +0700

    commit 10

--- PertemuanKeEnam/PertemuanKeEnam.cpp
+++ PertemuanKeEnam/PertemuanKeEnam.cpp
@@ -50,4 +50,12 @@ void q_short(int low, int high)
 	i = low + 1; //step 3
 	j = high;  //step 4
 
-	
+	while (i <= j) //step 10
+	{
+		//step for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) //step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
+		
